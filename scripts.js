82d927b1b6415a8c74df22aea6098275a17c0ad1
Daniel Graham <dzgraham976@gmail.com>
2025-11-19 23:28:56 -0500

    Added button functionality and message response to form submission

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function(){
+    const allButtons = document.querySelectorAll('.btn');
+    const sub_button = allButtons[1];
+    const message = document.querySelector('.message');
+    const emailSub = document.getElementById("email");
+
+    sub_button.addEventListener("click", function(event){
+        const  email = emailSub.value.trim();
+        event.preventDefault();
+        if((email==='') || ((!(email.includes("@"))) || (!(email.endsWith(".com"))) ||(email.includes("@.com")) || (email.startsWith("@")) )){
+            message.style.color = 'red';
+            message.textContent = "Please enter a valid email address"; 
+        }else{
+            message.style.color = 'blue';
+            message.textContent = `Thank you! Your email address, ${email}, has been added to our mailing list!`;
+        }
+    })
+});
